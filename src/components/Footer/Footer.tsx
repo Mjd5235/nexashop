320c87b41ede8ddd46def4a1a227e52051353a30
Majd <programmermjd@gmail.com>
2026-09-25 17:13:14 +0300

    improve mobile best practices

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -41,7 +41,7 @@ export default function Footer() {
         </div>
         <div className={styles.appimgs}>
           <Image className={styles.apps} src={'/Application/google.webp'} width={150} height={45} alt='google' />
-          <Image className={styles.apps} src={'/Application/apple.webp'} width={150} height={45} alt='apple' />
+          <Image className={styles.apps} src={'/Application/apple.webp'} width={150} height={50.33} alt='apple' />
         </div>
       </div>
       <ul className={styles.footerlinks}>
